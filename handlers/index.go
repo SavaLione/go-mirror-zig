8c5b4021e32e2903ebefe3b76b9fa2c8b1c11060
Savelii Pototskii <savelii.pototskii@gmail.com>
2026-03-13 21:54:35 +0100

    Added server version and system architecture information to the index page (#19)

--- handlers/index.go
+++ handlers/index.go
@@ -4,16 +4,29 @@ import (
 	"html/template"
 	"log/slog"
 	"net/http"
+	"runtime"
 )
 
-func RootHandler(t *template.Template) http.HandlerFunc {
+func RootHandler(t *template.Template, version string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if r.URL.Path != "/" {
 			http.NotFound(w, r)
 			return
 		}
 
-		if err := t.ExecuteTemplate(w, "index.html", nil); err != nil {
+		info := struct {
+			Version      string
+			Architecture string
+		}{
+			Version:      version,
+			Architecture: runtime.GOARCH,
+		}
+
+		if version == "" {
+			info.Version = "unknown"
+		}
+
+		if err := t.ExecuteTemplate(w, "index.html", info); err != nil {
 			slog.Error(
 				"failed to execute index template",
 				"error", err,
